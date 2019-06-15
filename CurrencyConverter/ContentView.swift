3158118b3d03466fd15f0a68be52af3c9047a72d
Robert Abramczyk <robert.abramczyk.1990@gmail.com>
2019-06-15 19:08:58 +0200

    Initial commit

---
+++ CurrencyConverter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CurrencyConverter
+//
+//  Created by Robert Abramczyk on 15/06/2019.
+//  Copyright © 2019 Robert Abramczyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
